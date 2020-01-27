552a9f73a9686f2bad8614199429056669d8282a
new2dmancode <60340189+new2dmancode@users.noreply.github.com>
2020-01-27 12:44:12 +0300

    Update names.py
    add cuntry

--- HomeWork/names.py
+++ HomeWork/names.py
@@ -1,7 +1,8 @@
 
 name = input("Give me your name: ")
 age = input("Give me your age: ")
+cuntry = input("Give me your cuntry: ")
 print("Your name is..: " + name)
 print("your age is..: " + age)
-
+print("your cuntry is..: " + cuntry)
 print('Hello ' + name)
